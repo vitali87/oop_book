e69cd75653c77a28898efa27fe10c279cd5b3d09
vitali87 <eheva87@gmail.com>
2021-12-19 16:58:55 +0000

    Adding more code examples

--- src/property.py
+++ src/property.py
@@ -60,7 +60,7 @@ class NorwegianBlue:
         return self._state
 
     def _set_state(self, state: str) -> None:
-        print(f"Setting {self._name}'s State")
+        print(f"Setting {self._name}'s State to {state!r}")
         self._state = state
 
     def _del_state(self) -> None:
@@ -71,3 +71,24 @@ class NorwegianBlue:
         _get_state, _set_state, _del_state,
         "This is a silly property"
     )
+
+
+class NorwegianBlue_P:
+    def __init__(self, name: str) -> None:
+        self._name = name
+        self._state: str
+
+    @property
+    def silly(self):
+        print(f"Getting {self._name}'s State")
+        return self._state
+
+    @silly.setter
+    def silly(self, state: str) -> None:
+        print(f"Setting {self._name}'s State to {state!r}")
+        self._state = state
+
+    @silly.deleter
+    def silly(self) -> None:
+        print(f"{self._name} is pushing up daisies!")
+        del self._state
